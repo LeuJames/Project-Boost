e8eb743bc8129abe9b920a2d4426a54ad5862cf0
James Leu <leujase@gmail.com>
2020-06-23 12:28:10 -0500

    added ship, about to add force

---
+++ Assets/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+  Rigidbody rigidody;
+    // Start is called before the first frame update
+    void Start()
+    {
+      rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+      if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
+      {
+        rigidbody.force.AddRelativeForce;
+      }
+      if (Input.GetKey(KeyCode.A))
+      {
+        print("rotating left");
+      }
+      else if (Input.GetKey(KeyCode.D))
+      {
+        print("rotating right");
+      }
+    }
+}
